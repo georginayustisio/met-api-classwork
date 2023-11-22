42d2937eb2b009b4c4882487330c70a2a1aa5412
Georgina <georginatania@georginas-mbp.mynetworksettings.com>
2023-11-22 18:10:26 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function fetchRandomArtwork() {
+    fetch('https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=11')
+        .then(response => response.json())
+        .then(data => {
+            if (data.total) {
+                // Generate a random index based on the number of objects
+                const randomIndex = Math.floor(Math.random() * data.total);
+                const objectID = data.objectIDs[randomIndex];
+                fetchArtworkDetails(objectID);
+            }
+        })
+        .catch(error => console.error('Error fetching object IDs:', error));
+}
+
+function fetchArtworkDetails(objectID) {
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectID}`)
+        .then(response => response.json())
+        .then(artwork => {
+            displayArtwork(artwork);
+        })
+        .catch(error => console.error('Error fetching artwork:', error));
+}
+
+function displayArtwork(artwork) {
+    if (artwork.primaryImage && artwork.title && artwork.artistDisplayName) {
+        document.getElementById('artwork-image').src = artwork.primaryImage;
+        document.getElementById('artwork-title').textContent = `Title: ${artwork.title}`;
+        document.getElementById('artist-name').textContent = `Artist: ${artwork.artistDisplayName}`;
+    } else {
+        document.getElementById('artwork-container').innerHTML = '<p>Artwork not found.</p>';
+    }
+}
+
+// Call the fetchRandomArtwork function when the page loads
+window.onload = fetchRandomArtwork;
